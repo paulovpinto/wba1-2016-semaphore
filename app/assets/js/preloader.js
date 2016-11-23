81eea65ea96b6e5f8efc6472ef83a18c060c1d3f
Christian Noss <christian.noss@fh-koeln.de>
2016-11-23 23:23:30 +0100

    Devmode geprüft

--- app/assets/js/preloader.js
+++ app/assets/js/preloader.js
@@ -16,8 +16,8 @@ der die Startseite läd.
 
 // cn: Sorgt für Consolenausgaben und Helper Menü 
 var devmode = true;
-if(location.href.match(/christiannoss\.de/)){ devmode = false; }
-
+if(location.href.match(/christiannoss/)){ devmode = false; }
+console.log(location.href);
 // In diesem Objekt werdne die Templates gespeichert
 var templates = {};
 
